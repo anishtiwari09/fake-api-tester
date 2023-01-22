3f586a92678cfbdc89a130eaa32336b031da9c92
anishtiwari09 <aneeshtiwari24@gmail.com>
2023-01-22 11:33:26 +0530

    deploy

--- Data/CheckInData/checkIn.js
+++ Data/CheckInData/checkIn.js
@@ -1,152 +1,370 @@
 function checkInData() {
-  return {
+  return  {
     status: true,
-    activity_id: 15,
-    name: "Affirmation",
-    category: "Self",
+    activity_id: 19,
+    name: "Feeling",
+    category: "Feeling",
     total_count: 0,
     teacher_instruction:
-      "Our thoughts have a lot of power. It defines how we feel about ourselves. So let us start our session with some positive thoughts. Here are two options on your screen. Which positive thought would you pick today?",
+      "Let us see How are you are feeling today? There are some feeling on your screen. Please pick a feeling that you can relate to at this point of time. For example, if you are feeling 'happy' then take that feeling and put it in the empty box that see in your screen.",
     teacher_instruction1: null,
     teacher_instruction2: null,
     teacher_instruction3: null,
     teacher_instruction4: null,
-    student_instruction:
-      "Let us start our session with some positive thoughts. Here are two options on your screen. Which positive thought would you pick today?",
-    student_instruction2: null,
+    student_instruction: "how are you feeling",
+    student_instruction2:
+      "Pick a feeling from the given feelings on your screen and drag it in the empty box.",
     student_instruction3: null,
     student_instruction4: null,
     student_instruction5: null,
     activity_data: [
       {
-        category_id: 21,
-        name: "I am unique",
-        image:
-          "https://check-in-activity.s3.ap-south-1.amazonaws.com/unique_girl.png",
+        category_id: 61,
+        name: "Happy",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Happy.png",
         gif_image:
-          "https://check-in-activity.s3.ap-south-1.amazonaws.com/unique_girl.gif",
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Happy.gif",
         story_question_data: [
           {
-            activity_data_id: 179,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/01.png",
+            activity_data_id: 91,
+            question: "How are you feeling today?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 180,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/02.png",
+            activity_data_id: 92,
+            question: "What has made you happy?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 181,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/03.png",
+            activity_data_id: 93,
+            question: "What other things make you happy?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 182,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/04.png",
-            answer: "",
-          },
-          {
-            activity_data_id: 183,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/05.png",
-            answer: "",
-          },
-          {
-            activity_data_id: 184,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/06.png",
-            answer: "",
-          },
-          {
-            activity_data_id: 185,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/07.png",
-            answer: "",
-          },
-          {
-            activity_data_id: 186,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/unique/08.png",
+            activity_data_id: 94,
+            question: "What do you do when you are happy?",
+            story: "",
             answer: "",
           },
         ],
         count: null,
       },
       {
-        category_id: 22,
-        name: "I am kind to everyone",
-        image:
-          "https://check-in-activity.s3.ap-south-1.amazonaws.com/kind_girl.png",
+        category_id: 62,
+        name: "Sad",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sad.png",
         gif_image:
-          "https://check-in-activity.s3.ap-south-1.amazonaws.com/kind_girl.gif",
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/sad.gif",
         story_question_data: [
           {
-            activity_data_id: 203,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/01.jpg",
+            activity_data_id: 95,
+            question:
+              "What is the emotion that you pick today that would best describe you",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 204,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/02.jpg",
+            activity_data_id: 96,
+            question: "How do you know that you are sad today?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 205,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/03.jpg",
+            activity_data_id: 97,
+            question:
+              "What would you wish that it was different so that you don't feel this way?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 206,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/04.jpg",
+            activity_data_id: 98,
+            question: "What can help you feel better?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 63,
+        name: "Angry",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Angry.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Angry.gif",
+        story_question_data: [
+          {
+            activity_data_id: 99,
+            question: "What has made you angry - is it a person or a situation",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 207,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/05.jpg",
+            activity_data_id: 100,
+            question: "What do you feel like doing when you are angry?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 208,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/06.jpg",
+            activity_data_id: 101,
+            question: "When did you realise that you are angry?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 209,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/07.jpg",
+            activity_data_id: 102,
+            question:
+              "What will help you to calm down and let go of this feeling?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 64,
+        name: "Proud",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Proud.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Proud.gif",
+        story_question_data: [
+          {
+            activity_data_id: 103,
+            question: "How do you explain this feeling?",
+            story: "",
             answer: "",
           },
           {
-            activity_data_id: 210,
-            question: "",
-            story:
-              "https://check-in-activity.s3.ap-south-1.amazonaws.com/affirmation/kind/08.jpg",
+            activity_data_id: 104,
+            question: "What has lead to you feeling this way?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 105,
+            question: "Is there anyone else who is sharing this feeling with you",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 106,
+            question: "What else this it make you feel?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 65,
+        name: "Scared",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Scared.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Scared.gif",
+        story_question_data: [
+          {
+            activity_data_id: 107,
+            question: "Why are you scared?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 108,
+            question: "What/who has scared you?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 109,
+            question: "What do you feel when you are scared?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 110,
+            question: "How and who else help you comfort you?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 66,
+        name: "Excited",
+        image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Excited.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Excited.gif",
+        story_question_data: [
+          {
+            activity_data_id: 111,
+            question: "That’s great new, what are you excited about?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 112,
+            question: "How do you feel when you are excited?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 113,
+            question: "How do you feel when you are excited?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 114,
+            question: "Who all do you want to share your excitement with?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 115,
+            question: "What do you feel like doing when you are so excited?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 67,
+        name: "Sleepy",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sleepy.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sleepy.gif",
+        story_question_data: [
+          {
+            activity_data_id: 116,
+            question: "Why are you sleepy? Have you been sleeping well?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 117,
+            question: "What do you do when you feel sleey but you can't sleep",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 118,
+            question: "When do you feel most sleepy, other than nights?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 119,
+            question: "Do you like this feeling?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 68,
+        name: "Sick",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sick.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sick.gif",
+        story_question_data: [
+          {
+            activity_data_id: 120,
+            question:
+              "Why do you feel Sick? Are you sick of something or are you unwell?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 121,
+            question: "How do you feel when you feel sick?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 122,
+            question: "What do you do when you feel sick?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 123,
+            question: "What will help you feel sick?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 69,
+        name: "Nervous",
+        image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Nervous.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Nervous.gif",
+        story_question_data: [
+          {
+            activity_data_id: 124,
+            question: "How do you feel when you are nervous?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 125,
+            question: "Does it manifect in any kind of physical reaction?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 126,
+            question: "What are you nervous about?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 127,
+            question: "Have you told anyone about it? What can help you?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 70,
+        name: "Guilty",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Guilty.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Guilty.gif",
+        story_question_data: [
+          {
+            activity_data_id: 128,
+            question: "Why do you feel guilty and what are you guilty of?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 129,
+            question: "Is it a good feeling or not so good feeling? And why?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 130,
+            question: "Can you share this feeling with anyone?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 131,
+            question: "What are the other feelings associated with this?",
+            story: "",
             answer: "",
           },
         ],
