72d4f72695ed280dae8169867436ccca8aa85b66
anishtiwari09 <aneeshtiwari24@gmail.com>
2023-01-22 23:36:04 +0530

    feeling checkout data

--- Data/CheckoutData/CheckoutData.js
+++ Data/CheckoutData/CheckoutData.js
@@ -1,42 +1,444 @@
 function CheckoutData() {
-  return {
+  return{
     status: true,
     student_activity_data: [
       {
-        student_checkin_activity_id: 277,
-        checkin_out_activity_id: 46,
-        response: "/system/user_checkin_out_activity/response/277/image.png",
-        gif_image: "",
-        checkin_out_activity_category_id: "",
-        selected_checkin_name: "",
-        selected_checkin_path: "",
+        student_checkin_activity_id: 346,
+        checkin_out_activity_id: 19,
+        response: "",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sick.gif",
+        checkin_out_activity_category_id: 68,
+        selected_checkin_name: "Sick",
+        selected_checkin_path:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sick.png",
       },
     ],
-    activity_id: 47,
-    name: "Shape",
-    category: "Circle & Rectangle",
-    total_count: 5,
+    activity_id: 18,
+    name: "Feeling",
+    category: "Feeling",
+    total_count: 0,
     instruction:
-      "At the beginning of the session you did a great job by drawing so creatively. There is something that I would want to draw for you, as we end the session. Hope you like it!",
-    teacher_instruction1: null,
-    student_instruction: null,
+      "We have completed our class for today. If you remember, before the class started, you were feeling.",
+    teacher_instruction1:
+      "Let us see how you are feeling after the class. On your screen, please find the same set of feelings.\u003cbr\u003ePlease pick one that you can relate to and put it in the empty box.",
+    teacher_instruction2: null,
+    teacher_instruction3: null,
+    teacher_instruction4: null,
+    student_instruction: "You were feeling",
+    student_instruction2: "How are you feeling now?",
+    student_instruction3:
+      "Pick a feeling from the given feelings on your screen and drag it in the empty box.",
+    student_instruction4: null,
+    student_instruction5: null,
     activity_data: [
       {
-        category_id: 109,
-        name: "Circle",
-        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/circle.png",
-        gif_image: null,
-        story_question_data: [],
-        count: 3,
+        category_id: 51,
+        name: "Happy",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Happy.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Happy.gif",
+        story_question_data: [
+          {
+            activity_data_id: 132,
+            question: "How do you feel after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 133,
+            question:
+              "Is there anything that has happedned in the class that keeps you feeling (excited/happy)",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 134,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 135,
+            question: "Do you know what made you feel better?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
       },
       {
-        category_id: 110,
-        name: "Rectangle",
+        category_id: 52,
+        name: "Sad",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sad.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/sad.gif",
+        story_question_data: [
+          {
+            activity_data_id: 144,
+            question: "How are you feeling now? Do you feel any better?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 145,
+            question:
+              "Is there anything that happened during the class which keeps you feeling sad?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 146,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 147,
+            question:
+              "Is there something that has happened in the class which could have triggered this?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 148,
+            question:
+              "Is there any way that I can help you? Do you want to talk?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 53,
+        name: "Angry",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Angry.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Angry.gif",
+        story_question_data: [
+          {
+            activity_data_id: 149,
+            question: "How are you feeling now? Do you feel any better?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 150,
+            question:
+              "Is there anything that happened during the class which keeps you feeling sad?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 151,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 152,
+            question:
+              "Is there something that has happened in the class which could have triggered this?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 153,
+            question:
+              "Is there any way that I can help you? Do you want to talk?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 54,
+        name: "Proud",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Proud.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Proud.gif",
+        story_question_data: [
+          {
+            activity_data_id: 136,
+            question: "How do you feel after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 137,
+            question:
+              "Is there anything that has happedned in the class that keeps you feeling (excited/happy)",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 138,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 139,
+            question: "Do you know what made you feel better?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 55,
+        name: "Scared",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Scared.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Scared.gif",
+        story_question_data: [
+          {
+            activity_data_id: 154,
+            question: "How are you feeling now? Do you feel any better?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 155,
+            question:
+              "Is there anything that happened during the class which keeps you feeling sad?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 156,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 157,
+            question:
+              "Is there something that has happened in the class which could have triggered this?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 158,
+            question:
+              "Is there any way that I can help you? Do you want to talk?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 56,
+        name: "Excited",
         image:
-          "https://check-in-activity.s3.ap-south-1.amazonaws.com/rectangle.png",
-        gif_image: null,
-        story_question_data: [],
-        count: 2,
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Excited.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Excited.gif",
+        story_question_data: [
+          {
+            activity_data_id: 140,
+            question: "How do you feel after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 141,
+            question:
+              "Is there anything that has happedned in the class that keeps you feeling (excited/happy)",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 142,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 143,
+            question: "Do you know what made you feel better?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 57,
+        name: "Sleepy",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sleepy.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sleepy.gif",
+        story_question_data: [
+          {
+            activity_data_id: 159,
+            question: "How are you feeling now? Do you feel any better?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 160,
+            question:
+              "Is there anything that happened during the class which keeps you feeling sad?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 161,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 162,
+            question:
+              "Is there something that has happened in the class which could have triggered this?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 163,
+            question:
+              "Is there any way that I can help you? Do you want to talk?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 58,
+        name: "Sick",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sick.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Sick.gif",
+        story_question_data: [
+          {
+            activity_data_id: 164,
+            question: "How are you feeling now? Do you feel any better?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 165,
+            question:
+              "Is there anything that happened during the class which keeps you feeling sad?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 166,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 167,
+            question:
+              "Is there something that has happened in the class which could have triggered this?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 168,
+            question:
+              "Is there any way that I can help you? Do you want to talk?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 59,
+        name: "Nervous",
+        image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Nervous.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Nervous.gif",
+        story_question_data: [
+          {
+            activity_data_id: 169,
+            question: "How are you feeling now? Do you feel any better?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 170,
+            question:
+              "Is there anything that happened during the class which keeps you feeling sad?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 171,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 172,
+            question:
+              "Is there something that has happened in the class which could have triggered this?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 173,
+            question:
+              "Is there any way that I can help you? Do you want to talk?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
+      },
+      {
+        category_id: 60,
+        name: "Guilty",
+        image: "https://check-in-activity.s3.ap-south-1.amazonaws.com/Guilty.png",
+        gif_image:
+          "https://check-in-activity.s3.ap-south-1.amazonaws.com/Guilty.gif",
+        story_question_data: [
+          {
+            activity_data_id: 174,
+            question: "How are you feeling now? Do you feel any better?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 175,
+            question:
+              "Is there anything that happened during the class which keeps you feeling sad?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 176,
+            question: "How are you feeling after the session?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 177,
+            question:
+              "Is there something that has happened in the class which could have triggered this?",
+            story: "",
+            answer: "",
+          },
+          {
+            activity_data_id: 178,
+            question:
+              "Is there any way that I can help you? Do you want to talk?",
+            story: "",
+            answer: "",
+          },
+        ],
+        count: null,
       },
     ],
     message: "Checkin Activity sent successfully",
